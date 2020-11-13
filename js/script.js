29b8b69b7a8ab92b9152f8746d1acbcc9969358d
OisinMc1 <44812591+OisinMc1@users.noreply.github.com>
2020-11-13 10:06:43 +0000

    test commit

--- js/script.js
+++ js/script.js
@@ -1,127 +1,50 @@
-const baseURL = 'http://image.tmdb.org/t/p/'
+$(document).ready(function () {
+    $("#submit").click(function (e) {
+        var validate = Validate();
+        $("#message").html(validate);
+        if (validate.length == 0) {
+            Search(1);
+        }
+    });
 
-function getRecomendations (item) {
-  const url = `https://api.themoviedb.org/3/movie/${item.id}/recommendations?api_key=${APIKEY}`
+    function Search(page) {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/movie?language=en-US&query=" + $("#searchInput").val() + "&page=" + page + "&include_adult=false",
+            data: { "api_key": "53e9e51b8f17da47596865a5b4bf906f" },
+            dataType: "json",
+            success: function (result, status, xhr) {
+                var resultHtml = $("<div class=\"resultDiv\"><p>Names</p>");
+                for (i = 0; i < result["results"].length; i++) {
 
-  fetch(url)
-    .then(data => {
-      return data.json()
-    })
-    .then(res => {
-      let items = res.results
-      var recommendations = document.getElementById('recommendations')
+                    var image = result["results"][i]["poster_path"] == null ? "Image/no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["results"][i]["poster_path"];
 
-      for (var i = 0; i < items.length; i++) {
-        let item = items[i]
-        let tmpl = document.getElementById('recomendationsTemplate')
-        tmpl.querySelector('.movie-poster').src = item.poster_path ? `${baseURL}w154/${item.poster_path}` : 'https://via.placeholder.com/154x239.png?text=NO+IMAGE'
-        tmpl.querySelector('.movie-title').innerText = item.original_title
-        tmpl.querySelector('.item').onclick = setMovie.bind(event, item)
-      }
-    })
-}
+                    resultHtml.append("<div class=\"result\" resourceId=\"" + result["results"][i]["id"] + "\">" + "<img src=\"" + image + "\" />" + "<p><a>" + result["results"][i]["title"] + "</a></p></div>")
+                }
 
-function search (page) {
-  let searchInput = document.getElementById('searchInput').value
+                resultHtml.append("</div>");
+                $("#message").html(resultHtml);
 
-  if (!searchInput) {
-    setTitle()
-    setMoviesTable(mostPopularMovies)
-    return
-  }
-
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&page=${page}&query=${searchInput}`
-
-  fetch(url)
-    .then(data => {
-      return data.json()
-    })
-    .then(res => {
-      setMoviesTable(res.results)
-    })
-}
-
-function setBannerImage (res) {
-  let randomItem = res[Math.floor(Math.random() * 20 + 1)]
-  let header = document.getElementById('header')
-  header.style.background = randomItem ? `linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)), url(${baseURL}original/${randomItem.backdrop_path}) no-repeat top` : '#78909C'
-  header.style.backgroundSize = 'cover'
-}
-
-function setMoviesTable (items) {
-  clearMoviesTable()
-  var moviesTable = document.getElementById('moviesTable')
-
-  for (var i = 0; i < items.length; i++) {
-    let item = items[i]
-    let tmpl = document.getElementById('movieListTemplate').content.cloneNode(true)
-    tmpl.querySelector('.movie-poster').src = item.poster_path ? `${baseURL}w45/${item.poster_path}` : 'https://via.placeholder.com/45x68.png?text=NO+IMAGE'
-    tmpl.querySelector('.movie-title').innerText = item.original_title
-    tmpl.querySelector('.movie-release-year').innerText = item.release_date.slice(0, 4)
-    tmpl.querySelector('.item').onclick = setMovie.bind(event, item)
-    moviesTable.appendChild(tmpl)
-  }
-}
-
-function setMovie (item) {
-  clearMoviesTable()
-  clearTitle()
-  clearMovie()
-  clearRecommendation()
-
-  getRecomendations(item)
-
-  var movie = document.getElementById('movie')
-  var tmpl = document.getElementById('movieTemplate').content.cloneNode(true)
-  tmpl.querySelector('.title').innerText = item.original_title
-  tmpl.querySelector('.movie-release-year').innerText = item.release_date.slice(0, 4)
-  tmpl.querySelector('.movie-poster').src = item.poster_path ? `${baseURL}w342/${item.poster_path}` : 'https://via.placeholder.com/342x513.png?text=NO+IMAGE'
-  tmpl.querySelector('.overview').innerText = item.overview
-  movie.appendChild(tmpl)
-}
-
-function setTitle () {
-  var title = document.getElementById('title')
-  var tmpl = document.getElementById('titleTemplate')
-  alert("Please Enter Valis Movie Title");
-}
-
-function clearMoviesTable () {
-  moviesTable.innerHTML = ''
-}
-
-function clearTitle () {
-  title.innerHTML = ''
-}
-
-function clearMovie () {
-  movie.innerHTML = ''
-}
-
-function clearRecommendation () {
-  recommendations.innerHTML = ''
-}
-
-window.onload = function() {
-  getTrendingList()
-
-  // Execute a Search function when the user presses Enter key
-  let searchInput = document.getElementById("searchInput")
-
-  searchInput.addEventListener("keyup", (event) => {
-    if (event.keyCode === 13) {
-      search()
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
     }
 
-    if (!searchInput.value) {
-      clearRecommendation()
-      clearTitle()
-      clearMovie()
-
-      setTitle()
-      setMoviesTable(mostPopularMovies)
+    function Validate() {
+        var errorMessage = "";
+        if ($("#searchInput").val() == "") {
+            errorMessage += "► Enter Search Text";
+        }
+        return errorMessage;
     }
-  })
 
-  searchInput.focus()
-}
+
+    $(document).ajaxStart(function () {
+        $(".imageDiv img").show();
+    });
+
+    $(document).ajaxStop(function () {
+        $(".imageDiv img").hide();
+    });
+});
